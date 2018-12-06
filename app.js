aea21b723fb26b1be16d277f9bc2a3bef4052383
Cook1619 <cook6348@gmail.com>
2018-12-06 07:38:18 -0600

    Project set up with express and body-parser

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+})
+
+
